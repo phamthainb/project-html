57dfe5e44e4d4f9a50898d50fc76a617861fdb52
phamthainb <phamthai4643@gmail.com>
2019-11-18 17:33:43 +0700

    code in html-2

--- html-2/app/js/main.js
+++ html-2/app/js/main.js
@@ -6,44 +6,4 @@ $(document).ready(function () {
         duration: "300",
     });
 
-    if ($(this).scrollTop() >= 42) {
-        $(".header-nav").addClass("scrolled");
-    } else {
-        $(".header-nav").removeClass("scrolled");
-    }
-    $(window).on("load", function () {
-        if ($(this).scrollTop() >= 42) {
-            $(".header-nav").addClass("scrolled");
-
-
-        } else {
-            $(".header-nav").removeClass("scrolled");
-
-        }
-    });
-    $(document).scroll(function () {
-        if ($(this).scrollTop() >= $("#main").offset().top) {
-            $(".back-top").addClass("active");
-
-        } else {
-            $(".back-top").removeClass("active");
-
-        }
-        if ($(this).scrollTop() >= 42) {
-            $("#header").addClass("scrolled");
-
-
-        } else {
-            $("#header").removeClass("scrolled");
-
-        }
-    });
-    $(".back-top").on("click", function () {
-        $(".back-top").removeClass("active");
-        $("html, body").animate({
-            scrollTop: 0
-        }, 1000);
-    });
-
-
 });
