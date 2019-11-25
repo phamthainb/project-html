8b447cf90a270b6fcc7d205ae64982df67dd8576
phamthainb <phamthai4643@gmail.com>
2019-11-26 02:25:17 +0700

    responsive 768

--- html-2/app/js/main.js
+++ html-2/app/js/main.js
@@ -5,5 +5,25 @@ $(document).ready(function () {
         easing: 'ease-in-out-cubic',
         duration: "300",
     });
+    // scroll header
+    // var lastScrollTop = 0;
+    // document.addEventListener("scroll", function () {
+    //     $("#header").addClass("scrolled");
+    //     var st = window.pageYOffset || document.documentElement.scrollTop;
+    //     if (st > lastScrollTop) {
+    //        setTimeout(()=>{
+    //         $("#header").addClass("scroll--down");
+    //         $("#header").removeClass("scroll--up");
+    //        }, 3000)
+    //     } else {
+    //         $("#header").addClass("scroll--up");
+    //         $("#header").removeClass("scroll--down");
+    //     }
+    //     lastScrollTop = st <= 0 ? 0 : st;
+    // }, false);
 
+    // nav mobile
+    $(".open--nav").toggle(()=>{
+       console.log("aa");
+    });
 });
