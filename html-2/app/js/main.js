1f274bf56905b421cd5cc51cad7366d58b5ca5d9
phamthainb <phamthai4643@gmail.com>
2019-12-10 11:33:59 +0700

    responsive in html-2

--- html-2/app/js/main.js
+++ html-2/app/js/main.js
@@ -1,29 +1,32 @@
 $(document).ready(function () {
     AOS.init({
-        once: true,
-        offset: 0,
-        easing: 'ease-in-out-cubic',
-        duration: "300",
     });
-    // scroll header
-    // var lastScrollTop = 0;
-    // document.addEventListener("scroll", function () {
-    //     $("#header").addClass("scrolled");
-    //     var st = window.pageYOffset || document.documentElement.scrollTop;
-    //     if (st > lastScrollTop) {
-    //        setTimeout(()=>{
-    //         $("#header").addClass("scroll--down");
-    //         $("#header").removeClass("scroll--up");
-    //        }, 3000)
-    //     } else {
-    //         $("#header").addClass("scroll--up");
-    //         $("#header").removeClass("scroll--down");
-    //     }
-    //     lastScrollTop = st <= 0 ? 0 : st;
-    // }, false);
 
+    // header
+    let lastScrollTop = 0;
+    document.addEventListener("scroll", function () {
+        let st = window.pageYOffset || document.documentElement.scrollTop;
+        let Header = $("#header");
+        if (st > lastScrollTop) {
+            Header.addClass("scroll");
+            lastScrollTop > 800 ? (
+                Header.addClass("hidden")
+            ) : ({})
+        } else { // top
+            Header.removeClass("hidden");
+            if(lastScrollTop == 100) Header.removeClass("scroll");
+        }
+        lastScrollTop = st <= 0 ? 0 : st;
+    }, false);
     // nav mobile
-    $(".open--nav").toggle(()=>{
-       console.log("aa");
+    let closeBtn = $(".close--nav");
+    let openBtn = $(".open--nav");
+    let navList = $(".nav-list");
+    ///
+    closeBtn.click(()=>{
+        navList.removeClass("nav-appear");
     });
+    openBtn.click(()=>{
+        navList.toggleClass("nav-appear");
+    })
 });
